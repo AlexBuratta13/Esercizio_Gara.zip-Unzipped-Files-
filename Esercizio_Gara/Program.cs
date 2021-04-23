76e826e1a6eb3925f92bc3ac7a0006e388e3b662
AlexBuratta13 <alex.buratta13@gmail.com>
2021-04-23 09:02:34 +0200

    Add project files.

---
+++ Esercizio_Gara/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Esercizio_Gara
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Inserisci numero partecipanti: ");
+            int p = int.Parse(Console.ReadLine());
+            List<string> partecipanti = new List<string>();
+            for (int i = 1; i <= p; i++)
+            {
+                Console.WriteLine($"Inserisci nome del corridore {i}: ");
+                string nome = Console.ReadLine();
+                partecipanti.Add(nome);
+                Corri(nome);
+            }
+            Console.ReadLine();
+        }
+
+        private static async Task Corri(string nome)
+        {
+            await Task.Run(() =>
+            {
+                for (int i = 1; i < 100; i++)
+                {
+                    Console.WriteLine($"{nome} è al km {i}.");
+                }
+                Console.WriteLine($"{nome} è arrivato al km 100 e ha finito la corsa");
+            });
+        }
+    }
+}
